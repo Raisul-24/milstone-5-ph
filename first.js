3bd14d6c3bb402d231db2cf703a6ef97573b03fd
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-12-17 22:53:35 +0600

    complete 5 milstone

---
+++ first.js
@@ -0,0 +1,13 @@
+console.log("First File");
+
+const allli = document.getElementsByTagName('li');
+console.log(allli);
+
+const alltitle = document.getElementsByClassName('section-title');
+console.log(alltitle);
+
+const secondSection = document.getElementById('second-section');
+console.log(secondSection);
+
+secondSection.style.color="Red";
+secondSection.style.background= "yellowgreen";
---
+++ second.js
@@ -0,0 +1,9 @@
+console.log("Second File");
+
+
+const secondList =document.getElementById('second-lust');
+
+
+const li = document.createElement('li');
+li.innerText = 'My Dynamic Item'
+secondList.appendChild(li)
